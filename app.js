7c549c9b9624ce113b24ef1f31e4c4215beb86d2
Rian Priansyah <rian.priansyah04@gmail.com>
2024-10-27 13:06:07 +0700

    menambahkan function addTodo dan create element

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ const filterInput = document.querySelector("#filter-input");
 const todoList = document.querySelector("#todo-list");
 const clearButton = document.querySelector("#clear-todos");
 
-console.log(clearButton);
+const addTodo = (e) => {
+  e.preventDefault();
+
+  // Membuat li element
+  const li = document.createElement("li");
+
+  // Menambahkan properti class pada li element
+  li.className = "list-group-item d-flex justify-content-between align-items-center mb-1 todo-item";
+
+  // Menambahkan children ke dalam li element
+  li.appendChild(document.createTextNode("Value dari task input"));
+
+  // Membuat delete button
+  const a = document.createElement("a");
+  a.href = "#";
+  a.className = "badge badge-danger delete-todo";
+  a.innerHTML = "Delete";
+
+  // Menambahkan children a ke dalam li element
+  li.appendChild(a);
+};
+
+todoForm.addEventListener("submit", addTodo);
